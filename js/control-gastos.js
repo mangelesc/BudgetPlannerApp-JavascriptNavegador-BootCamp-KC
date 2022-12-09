61c3786314c78d606384d1a4b27ba77e83331613
mangelesc <103500921+mangelesc@users.noreply.github.com>
2022-12-09 21:26:33 +0100

    función eliminar añadida

--- js/control-gastos.js
+++ js/control-gastos.js
@@ -1,5 +1,7 @@
 const activityTable = document.querySelector('#activityTable');
 const currentBalanceP = document.querySelector('#currentBalance');
+const totalIncomesDiv = document.querySelector('#totalIncomes');
+const totalExpensesDiv = document.querySelector('#totalExpenses');
 
 const addIncomeAmount = document.querySelector('#incomeAmount');
 const addIncomeDescript = document.querySelector('#incomeDescription');
@@ -17,21 +19,21 @@ let incomesTotal = 0;
 let expensesTotal = 0;
 
 
-/**Mostrar balance actual */ 
+//Mostrar balance actual 
 const showCurrentBalance = () => {
     currentBalanceP.innerHTML = "";
     savingTotal = 0;
-    console.log(savingTotal)
     arrayTransactions.forEach(transaction => {
         savingTotal += parseFloat(transaction.amount);
-        console.log(savingTotal)
     })
-    currentBalanceP.innerHTML = savingTotal;
+    currentBalanceP.innerHTML = savingTotal + ' €';
 }
 
 
 //Calcular incomes y expenses 
-const showIncomesExpenses = () => {
+const calculateIncomesExpenses = () => {
+    incomesTotal = 0;
+    expensesTotal = 0;
     arrayTransactions.forEach(transaction => {
         let amount = parseFloat(transaction.amount);
         if (amount >= 0) {
@@ -41,49 +43,74 @@ const showIncomesExpenses = () => {
         }
     })
 }
+
+const showIncomes = () => {
+    totalIncomesDiv.innerHTML = incomesTotal + ' €';
+}
+
+const showExpenses = () => {
+    totalExpensesDiv.innerHTML = expensesTotal + ' €';
+}
+
+// Creamos la tabla para las transacciones
+const createTransactionTr = () => {
+    activityTable.innerHTML = '';
+    //Recorro el array al revés, para luego poner usar el ined de la fila en deleteTransaction
+    for(let i = arrayTransactions.length-1; i >= 0; i--){
+        const newExpense = document.createElement('tr');
+        let newRow = `<td>${arrayTransactions[i].description} </td>
+                    <td>${arrayTransactions[i].amount} €</td>
+                    <td onclick="deleteTransaction(this)"><img class="binIMG" src="imgs/bin.png" ></td>
+                    <td class="tableColor"></td>`;
+        newExpense.innerHTML = newRow;
+
+        activityTable.insertAdjacentHTML('beforeend', newRow);
+    }
+}; 
+
+// Llamamos a todas las funciones desde una sola
+const refresh = () => {
+    showCurrentBalance();
+    calculateIncomesExpenses();
+    showIncomes();
+    showExpenses();
+    createTransactionTr();
+}
+
 //Añadir un ingreso al array
 const addIncome = () => {
     const description = addIncomeDescript.value;
     let amount = addIncomeAmount.value;
-    amount = parseFloat(amount).toFixed(2);
-
-    arrayTransactions.push({description,amount});
-    console.log(arrayTransactions);
-    createTransactionTr()
-    showCurrentBalance(); 
+    if (amount>0 && amount!= NaN &&description != ''){
+        amount = parseFloat(amount).toFixed(2);
+        arrayTransactions.push({description,amount});
+        refresh();
+    } else {
+        alert("Ops, fields cannot be empty!\nMake sure you add a positive integer number in the amount section")
+    } 
 }
 
 //Añadir un gasto al array
 const addExpense = () => {
     const description = addExpenseDescript.value;
     let amount = addExpenseAmount.value;
-    amount =  parseFloat(-amount).toFixed(2);
-    arrayTransactions.push({description,amount});
-    console.log(arrayTransactions);
-    createTransactionTr()
-    showCurrentBalance(); 
+    if (amount>0 && amount!= NaN &&description != ''){
+        amount =  parseFloat(-amount).toFixed(2);
+        arrayTransactions.push({description,amount});
+        refresh();
+    } else {
+        alert("Ops, fields cannot be empty!\nMake sure you add a positive integer number in the amount section")
+    } 
 }
 
-// Creamos la tabla para las transacciones
-const createTransactionTr = () => {
-    activityTable.innerHTML = '';
-    arrayTransactions.forEach(transaction => {
-        const newExpense = document.createElement('tr');
-        let newRow = `<td>${transaction.description}</td>
-                    <td>${transaction.amount}</td>
-                    <td><img class="RRSS" src="imgs/bin.png"></td>
-                    <td class="tableColor"></td>`;
-        newExpense.innerHTML = newRow;
-
-        activityTable.insertAdjacentHTML('afterbegin', newRow);
-    })
-};    
-
-
 //Eliminar una transacción
+const deleteTransaction = (row) => {
+    let rowIndex = row.parentElement.rowIndex;
+    arrayTransactions.splice(rowIndex,1);
+    refresh();
+}
 
 // Event listeners y funciones
 addIncomeButton.addEventListener('click', addIncome);
 addExpenseButton.addEventListener('click', addExpense);
-
-createTransactionTr()
+refresh();
